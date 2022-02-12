4fe776133a74578262db05984d2774a50cddbcb8
Swann <princeapurbask@gmail.com>
2022-02-12 20:20:05 +0600

    Fake authentication bind

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,17 +3,25 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DashboardLayout from "../components/DashboardLayout.vue";
+import Surveys from "../views/Surveys.vue";
+import store from "../store";
 
 const routes = [
   {
     path: '/',
     redirect: '/dashboard',
     component: DashboardLayout,
+    meta: {requiresAuth: true},
     children: [
       {
         path: '/dashboard',
-        name: 'dashboard',
+        name: 'Dashboard',
         component: Dashboard,
+      },
+      {
+        path: '/surveys',
+        name: 'Surveys',
+        component: Surveys
       }
     ]
   },
@@ -32,5 +40,22 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    console.log('IF')
+    next({
+      name: 'Login',
+    })
+  } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) {
+    console.log('ELSE IF')
+    next({
+      name: 'Dashboard',
+    })
+  } else {
+    console.log('ELSE')
+    next()
+  }
 })
 export default router;
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -9,7 +9,7 @@
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
       <div class="px-4 py-6 sm:px-0">
-        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"/>
       </div>
       <!-- /End replace -->
     </div>
@@ -17,26 +17,16 @@
 </template>
 
 <script>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+import {Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
+import {BellIcon, MenuIcon, XIcon} from '@heroicons/vue/outline'
+import {computed} from "vue";
+import store from "../store";
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
+
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  {name: 'Your Profile', href: '#'},
+  {name: 'Settings', href: '#'},
+  {name: 'Sign out', href: '#'},
 ]
 
 export default {
@@ -51,13 +41,6 @@ export default {
     BellIcon,
     MenuIcon,
     XIcon,
-  },
-  setup() {
-    return {
-      user,
-      navigation,
-      userNavigation,
-    }
   },
   name: 'Dashboard',
 }
