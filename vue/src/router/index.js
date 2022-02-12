d80a61949ca33d8ae06b8dc6f1ba5a1bc25664dd
Swann <princeapurbask@gmail.com>
2022-02-12 18:26:12 +0600

    Build a dashboardLayout base

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,33 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import DashboardLayout from "../components/DashboardLayout.vue";
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
